26172a8d35628de759b0707454f8eaecbbfd48d0
APOORVKPATIDAR <130593347+APOORVKPATIDAR@users.noreply.github.com>
2025-05-19 22:30:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Flask in Docker with ASG and ALB!"
+    return "Hello from apoorv done  with ASG and ALB `:)"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
